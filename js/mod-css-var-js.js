22dc31e03c08dbdbef118644831392f5ef4b01f7
tgray <tony.gray@trademe.co.nz>
2018-11-27 09:17:21 +1300

    cooments

--- js/mod-css-var-js.js
+++ js/mod-css-var-js.js
@@ -2,15 +2,19 @@
 
 // console.log('I work');
 
+// get the footer
 const footer = document.querySelector('footer');
 console.log(footer);
 
+// get the inputs
 const inputs = [].slice.call(document.querySelectorAll('input'));
 console.log(inputs);
 
+// add eventListener to the inputs
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
+// If the input type is 'color' update the footer color variable otherwise update the padding
 function handleUpdate() {
   if (this.type === 'color') {
     footer.style.setProperty('--footer-color', this.value);
