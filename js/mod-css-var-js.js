c4e1b2bac8dc42f0a8bef6eeb04b4c40b4ad1b31
tgray <tony.gray@trademe.co.nz>
2018-12-10 09:44:25 +1300

    coming back to this

--- js/mod-css-var-js.js
+++ js/mod-css-var-js.js
@@ -6,7 +6,7 @@ const footer = document.querySelector('footer');
 
 // get the inputs
 const inputs = [].slice.call(document.querySelectorAll(['input', 'select']));
-// console.log(inputs);
+console.log(`These are the inputs ${inputs}`);
 
 // get the input id's
 const inputProperties = [];
@@ -17,10 +17,28 @@ inputs.forEach(input => inputProperties.push([input.id, input.value]));
 // console.log(inputProperties[0, 0]);
 // console.log(inputProperties[0]);
 
+console.log(`These are the input properties ${inputProperties}`);
+
+console.log(`These are the input ids ${inputProperties.id}`);
+
 // This works but it's not what i want, prob have to do a forEach and then filter?
 console.log(
-    inputProperties.filter((x, i) => i === 1)
+    `Input properties filter: ${inputProperties.filter((x, i) => i === 1)}`
 );
+
+// inputs.forEach(input => inputProperties.filter((x, i) => i === 1));
+
+// trying to do the filter from above with a value  10/12/18
+inputs.forEach(input => inputProperties.filter((x, i) => i === 1));
+
+const inputValues = [];
+inputs.forEach(input => inputValues.push(input.value));
+console.log(`These are the input values: ${inputValues}`);
+
+
+const inputIds = [];
+inputs.forEach(input => inputIds.push(input.id));
+console.log(`These are the input id's: ${inputIds}`);  // was up to here
 
 // add eventListener to the inputs
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
@@ -87,3 +105,7 @@ function handleUpdate() {
 }
 
 // handleUpdate();
+
+// if () {
+//     console.log('Now we are getting somewhere.');
+// }
