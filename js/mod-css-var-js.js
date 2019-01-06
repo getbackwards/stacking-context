3d32d1e064f92fa995300fc3e200de635971ca6e
tgray <tony.gray@trademe.co.nz>
2019-01-06 17:09:58 +1300

    change to switch and clean up file

--- js/mod-css-var-js.js
+++ js/mod-css-var-js.js
@@ -1,40 +1,25 @@
 'use strict';
 // get the footer
 const footer = document.querySelector('footer');
-// console.log(footer);
 
 // get the inputs
 const inputs = [].slice.call(document.querySelectorAll(['input', 'select']));
-
-const inputIds = [];
-
-inputs.forEach(input => inputIds.push([input.id]));
-
-
-// console.log(inputIds);
 
 // add eventListener to the inputs
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 
 function handleUpdate(e) {
-    if (this.id === 'colorvalue') {
-        footer.style.setProperty('--footer-color', this.value)
-    } else if (this.id === 'margin') {
-        footer.style.setProperty('--palatte-padding-left', this.value + 'px')
-    } else if (this.id === 'div-one') {
-        // footer.style.setProperty('--palatte-padding-left', this.value + 'px')
-        // document.getElementById("div1").style.color = "blue";
-        document.getElementById("div1").style.setProperty('--z-index-div-one', this.value);
-        console.log("div one input selected.");
-    } else {
-        console.log("Your action was not accounted for programmatically.");
+    switch (this.id) {
+        case 'colorvalue':
+            footer.style.setProperty('--footer-color', this.value);
+            break;
+        case 'margin':
+            footer.style.setProperty('--palatte-padding-left', this.value + 'px')
+            break;
+        case 'div-one':
+            document.getElementById("div1").style.setProperty('--z-index-div-one', this.value);
+            break;
+        default:
+            console.log("Your action was not accounted for programmatically.");
     }
 }
-// function handleUpdate() {
-//
-//     if (inputIds[0][0] === 'colorvalue') {
-//         console.log('This is the color input');
-//     } else if (inputIds[0][0] === 'margin') {
-//         console.log('This is the range input');
-//     }
-// }
